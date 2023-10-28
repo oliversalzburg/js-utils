5a66c6c975b5d3892f3358b44164f497f18bd01d
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-10-28 19:39:36 +0200

    feat: More types

--- source/core.ts
+++ source/core.ts
@@ -1,4 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 /**
  * Describes a function that is a constructor for T.
  */
 export type ConstructorOf<T = Record<string, unknown>> = new (...args: any[]) => T;
+
+/**
+ * Describes literally any function.
+ */
+export type AnyFunction = (...args: any[]) => any;
+
+/**
+ * Any constructor
+ */
+export type AnyConstructor = new (...args: any[]) => any;
+
+/**
+ * Describes a function returning an instance of T.
+ */
+export type FunctionReturning<T = any> = (...args: any[]) => T;
+
+/**
+ * Describes a class "mixin", which is a function that returns a dynamically
+ * constructed class, based on the passed parameters.
+ */
+export type Mixin<T extends FunctionReturning> = InstanceType<ReturnType<T>>;
