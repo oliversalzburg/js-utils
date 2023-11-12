3e6b8cc43197c0b73d24551d05985f2cea016e0d
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-11-12 22:40:13 +0100

    docs(async): Clean up

--- source/async.ts
+++ source/async.ts
@@ -1,5 +1,9 @@
 /**
- * Executes an asynchrous context and ignores any results.
+ * Wraps the given asynchronous function in a new function that will ignore any outcome of
+ * the context as it resolves.
+ *
+ * This can be useful when providing asynchonous event handler to `.addEventlistener()`,
+ * which only expects synchronous functions.
  * @param context The asynchronous function to exectute.
  * @returns Nothing
  * @group Async
@@ -17,13 +21,15 @@ export const prepareAsyncContext = (context: (...args: Array<any>) => Promise<un
  * in case the function failed.
  * @param executable The asynchronous function to execute.
  * @param to The result we want to return when the function errors.
+ * @template {unknown} TExecutableReturn The return type of the function.
+ * @template {unknown} TCoalesce The type of the object to coalesce to.
  * @returns Whatever the function resolved to, or the provided replacement,
  * in case the function failed.
  * @group Async
  */
-export const coalesceOnRejection = async <TExecutableReturn, T>(
+export const coalesceOnRejection = async <TExecutableReturn, TCoalesce>(
   executable: (...args: Array<unknown>) => Promise<TExecutableReturn>,
-  to: T,
+  to: TCoalesce,
 ) => {
   try {
     return await executable();
