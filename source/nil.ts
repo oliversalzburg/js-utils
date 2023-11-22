c1e141df22db35abf93b0ac40ce4825f3139d7e7
Oliver Salzburg <oliver.salzburg@gmail.com>
2023-11-22 17:05:20 +0100

    docs(nil): Add group, clean up type params

--- source/nil.ts
+++ source/nil.ts
@@ -18,27 +18,35 @@ export type Maybe<T> = T | Nil;
  * Check if something is nil.
  * Can be used as a typeguard.
  * @param subject - The subject that could be nil.
+ * @typeParam TSubject - The type of the subject.
  * @returns `true` if the subject is nil, `false` otherwise.
+ * @group Nullability
  */
-export function isNil<T>(subject: Maybe<T>): subject is Nil {
+export function isNil<TSubject>(subject: Maybe<TSubject>): subject is Nil {
   return subject === null || subject === undefined;
 }
 
 /**
  * Check if something is a concrete value of the given type.
  * Can be used as a typeguard.
- * @param nilable - The subject that could be nil.
- * @param InstanceType - The type to check against.
+ * @param subject - The subject that could be nil.
+ * @param Prototype - The prototype to check against.
+ * @typeParam TSubject - The type to check against.
  * @returns `true` if the input element matches the given type,
  * `false` otherwise.
+ * @group Nullability
  */
-export function is<T>(nilable: Maybe<T>, InstanceType: ConstructorOf<T>): nilable is T {
-  return !isNil(nilable) && nilable instanceof InstanceType;
+export function is<TSubject>(
+  subject: Maybe<TSubject>,
+  Prototype: ConstructorOf<TSubject>,
+): subject is TSubject {
+  return !isNil(subject) && subject instanceof Prototype;
 }
 
 /**
  * Thrown when an unexpected nil value was encountered.
  * @group Errors
+ * @group Nullability
  */
 export class UnexpectedNilError extends Error {
   /**
@@ -54,10 +62,12 @@ export class UnexpectedNilError extends Error {
  * Ensure that the passed subject is not nil; throw otherwise.
  * @param subject - A subject that is possible nil.
  * @param errorMessage - An optional error message to throw when the subject is nil.
+ * @typeParam TSubject - The type of the subject.
  * @returns The subject, if it isn't nil.
  * @throws {@linkcode UnexpectedNilError} When the subject is nil.
+ * @group Nullability
  */
-export function mustExist<T>(subject: Maybe<T>, errorMessage?: string): T {
+export function mustExist<TSubject>(subject: Maybe<TSubject>, errorMessage?: string): TSubject {
   if (isNil(subject)) {
     throw new UnexpectedNilError(errorMessage);
   }
@@ -67,9 +77,11 @@ export function mustExist<T>(subject: Maybe<T>, errorMessage?: string): T {
 /**
  * Ensure that the passed subject is not nil; throw otherwise.
  * @param subject - A subject that is possibly nil.
+ * @typeParam TSubject - The type of the subject.
  * @throws {@linkcode UnexpectedNilError} When the subject is nil.
+ * @group Nullability
  */
-export function assertExists<T>(subject: Maybe<T>): asserts subject is T {
+export function assertExists<TSubject>(subject: Maybe<TSubject>): asserts subject is TSubject {
   if (isNil(subject)) {
     throw new UnexpectedNilError();
   }
