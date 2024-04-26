2dd51450f7e0a4275689258c06b1f8d40bf65584
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-04-26 13:17:27 +0200

    docs: Add group

--- source/error/console.ts
+++ source/error/console.ts
@@ -3,6 +3,7 @@
  * and prints it to a {@linkcode !Console}.
  * @param console - The console to print errors to.
  * @returns A function that will print errors to the console.
+ * @group Errors
  */
 export const redirectErrorsToConsole = (console: Console): ((error: unknown) => void) => {
   const printErrorsToConsole = (error: unknown): void => {
