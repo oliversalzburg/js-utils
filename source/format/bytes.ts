7386ec60fd0158e9a77c9937b1e819c8dac1e1a7
Oliver Salzburg <oliver.salzburg@adesso.de>
2024-05-13 16:17:25 +0200

    fix(format): Leftovers from merge

--- source/format/bytes.ts
+++ source/format/bytes.ts
@@ -4,7 +4,7 @@
  */
 
 /**
- * Options for the `prettyBytes` function.
+ * Options for the `formatBytes` function.
  * @group Formatting
  */
 export interface FormatBytesOptions {
@@ -25,9 +25,9 @@ export interface FormatBytesOptions {
    * Format the number as [bits](https://en.wikipedia.org/wiki/Bit) instead of [bytes](https://en.wikipedia.org/wiki/Byte). This can be useful when, for example, referring to [bit rate](https://en.wikipedia.org/wiki/Bit_rate).
    * @example
    * ```
-   * import prettyBytes from 'pretty-bytes';
+   * import formatBytes from '@oliversalzburg/js-utils/format/bytes.js';
    *
-   * prettyBytes(1337, {bits: true});
+   * formatBytes(1337, {bits: true});
    * //=> '1.34 kbit'
    * ```
    */
@@ -37,12 +37,12 @@ export interface FormatBytesOptions {
    * Format the number using the [Binary Prefix](https://en.wikipedia.org/wiki/Binary_prefix) instead of the [SI Prefix](https://en.wikipedia.org/wiki/SI_prefix). This can be useful for presenting memory amounts. However, this should not be used for presenting file sizes.
    * @example
    * ```
-   * import prettyBytes from 'pretty-bytes';
+   * import formatBytes from '@oliversalzburg/js-utils/format/bytes.js';
    *
-   * prettyBytes(1000, {binary: true});
+   * formatBytes(1000, {binary: true});
    * //=> '1000 bit'
    *
-   * prettyBytes(1024, {binary: true});
+   * formatBytes(1024, {binary: true});
    * //=> '1 kiB'
    * ```
    */
@@ -54,13 +54,13 @@ export interface FormatBytesOptions {
    * If neither `minimumFractionDigits` or `maximumFractionDigits` are set, the default behavior is to round to 3 significant digits.
    * @example
    * ```
-   * import prettyBytes from 'pretty-bytes';
+   * import formatBytes from '@oliversalzburg/js-utils/format/bytes.js';
    *
    * // Show the number with at least 3 fractional digits
-   * prettyBytes(1900, {minimumFractionDigits: 3});
+   * formatBytes(1900, {minimumFractionDigits: 3});
    * //=> '1.900 kB'
    *
-   * prettyBytes(1900);
+   * formatBytes(1900);
    * //=> '1.9 kB'
    * ```
    */
@@ -72,13 +72,13 @@ export interface FormatBytesOptions {
    * If neither `minimumFractionDigits` or `maximumFractionDigits` are set, the default behavior is to round to 3 significant digits.
    * @example
    * ```
-   * import prettyBytes from 'pretty-bytes';
+   * import formatBytes from '@oliversalzburg/js-utils/format/bytes.js';
    *
    * // Show the number with at most 1 fractional digit
-   * prettyBytes(1920, {maximumFractionDigits: 1});
+   * formatBytes(1920, {maximumFractionDigits: 1});
    * //=> '1.9 kB'
    *
-   * prettyBytes(1920);
+   * formatBytes(1920);
    * //=> '1.92 kB'
    * ```
    */
@@ -88,12 +88,12 @@ export interface FormatBytesOptions {
    * Put a space between the number and unit.
    * @example
    * ```
-   * import prettyBytes from 'pretty-bytes';
+   * import formatBytes from '@oliversalzburg/js-utils/format/bytes.js';
    *
-   * prettyBytes(1920, {space: false});
+   * formatBytes(1920, {space: false});
    * //=> '1.9kB'
    *
-   * prettyBytes(1920);
+   * formatBytes(1920);
    * //=> '1.92 kB'
    * ```
    */
@@ -141,20 +141,20 @@ const toLocaleString = (
  * @group Formatting
  * @example
  * ```
- * import prettyBytes from '@oliversalzburg/js-utils/format/bytes.js';
+ * import formatBytes from '@oliversalzburg/js-utils/format/bytes.js';
  *
- * prettyBytes(1337);
+ * formatBytes(1337);
  * //=> '1.34 kB'
  *
- * prettyBytes(100);
+ * formatBytes(100);
  * //=> '100 B'
  *
  * // Display file size differences
- * prettyBytes(42, {signed: true});
+ * formatBytes(42, {signed: true});
  * //=> '+42 B'
  *
  * // Localized output using German locale
- * prettyBytes(1337, {locale: 'de'});
+ * formatBytes(1337, {locale: 'de'});
  * //=> '1,34 kB'
  * ```
  */
