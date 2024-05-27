35401ffdc75e7d7d38a512e50679514b7c950d5c
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-05-27 19:01:07 +0200

    ci: Fix wrong version in release

--- .scripts/manifest-version.cjs
+++ .scripts/manifest-version.cjs
@@ -56,7 +56,7 @@ function getNextVersion() {
     // the package might not have been published yet
   }
 
-  const version = getRootVersion();
+  const version = getRootVersion(options.canary ?? false);
 
   if (versions.some(v => v === version)) {
     // eslint-disable-next-line no-console
