33364bd95a9399f77eb4abcec28c1a630b697e2b
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-05-30 16:47:47 +0200

    test: Fix another broken Windows test

--- source/format/count.test.ts
+++ source/format/count.test.ts
@@ -2,7 +2,11 @@ import { expect } from "chai";
 import { it } from "mocha";
 import { formatCount } from "./count.js";
 
-it("formats counts as expected", () => {
+it("formats counts as expected", function () {
+  // Fails on NodeJS20 on Windows
+  // https://github.com/oliversalzburg/js-utils/actions/runs/9069549841/job/24919347525
+  this.timeout(5000);
+
   expect(formatCount(-1)).to.equal("-1");
   expect(formatCount(0)).to.equal("0");
   expect(formatCount(1)).to.equal("1");
