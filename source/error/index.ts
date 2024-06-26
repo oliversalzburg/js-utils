40c7ec8814709532a7f75f8f44e526701ff44f8a
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-06-26 18:54:31 +0200

    style: Fix sorting

--- source/error/index.ts
+++ source/error/index.ts
@@ -1,4 +1,5 @@
 export * from "./AbstractError.js";
+export * from "./console.js";
 export * from "./InternalError.js";
 export * from "./InvalidArgumentError.js";
 export * from "./InvalidOperationError.js";
@@ -6,4 +7,3 @@ export * from "./NotImplementedError.js";
 export * from "./PermissionViolationError.js";
 export * from "./ResourceConflictError.js";
 export * from "./UnknownError.js";
-export * from "./console.js";
