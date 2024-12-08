cb2d6fb4b5b68448b8212552262f9b92dc887ddf
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-12-08 22:06:09 +0100

    fix: Count parser is inconsistent with count formatter

--- source/format/count.ts
+++ source/format/count.ts
@@ -1,4 +1,4 @@
-import { mustExist } from "../data/nil.js";
+import { coalesceArray, mustExist } from "../data/nil.js";
 
 /**
  * Convert counts to a human readable string: `1234` → `1.234K`.
@@ -21,21 +21,36 @@ export const formatCount = (count: number | bigint, locale?: string): string => 
  * @returns The number represented by the input string.
  */
 export const parseCount = (count: string, locale?: string): number => {
-  const format = new Intl.NumberFormat(locale);
+  const format = new Intl.NumberFormat(locale, {
+    compactDisplay: "short",
+    maximumFractionDigits: 3,
+    notation: "compact",
+  });
   const parts = format.formatToParts(-12345.6);
+  const orders = coalesceArray(
+    Array.from({ length: 20 }).map(
+      (_, i) => format.formatToParts(Math.pow(10, i)).find(d => d.type === "compact")?.value ?? "",
+    ),
+  );
   const numerals = Array.from({ length: 10 }).map((_, i) => format.format(i));
   const index = new Map(numerals.map((d, i) => [d, i]));
   const minusSign = new RegExp(`[${mustExist(parts.find(d => d.type === "minusSign")).value}]`);
-  const group = new RegExp(`[${mustExist(parts.find(d => d.type === "group")).value}]`, "g");
   const decimal = new RegExp(`[${mustExist(parts.find(d => d.type === "decimal")).value}]`);
   const numeral = new RegExp(`[${numerals.join("")}]`, "g");
+  const order = new RegExp(`(${[...new Set(orders.filter(o => o !== ""))].join("|")})`, "g");
 
-  const DIRECTION_MARK = /\u061c|\u200e/g;
-  return +count
+  const DIRECTION_MARK = /\u061c|\u200e|\u200f/g;
+  let multiplier = 1;
+  const value = count
     .trim()
     .replace(DIRECTION_MARK, "")
-    .replace(group, "")
     .replace(decimal, ".")
     .replace(numeral, d => mustExist(index.get(d)).toString())
-    .replace(minusSign, "-");
+    .replace(minusSign, "-")
+    .replace(order, o => {
+      multiplier = Math.pow(10, orders.indexOf(o));
+      return "";
+    })
+    .replace(/[ \u00a0]/g, "");
+  return +value * multiplier;
 };
