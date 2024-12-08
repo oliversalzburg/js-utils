e8b2f9c24f89d4f45751b789192ccaacb47a513e
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-12-08 20:58:17 +0100

    feat: Number parser

--- source/format/count.test.ts
+++ source/format/count.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { it } from "mocha";
-import { formatCount } from "./count.js";
+import { formatCount, parseCount } from "./count.js";
 
 it("formats counts as expected", function () {
   // Fails on NodeJS20 on Windows
@@ -14,4 +14,28 @@ it("formats counts as expected", function () {
   expect(formatCount(100)).to.equal("100");
   expect(formatCount(1000)).to.equal("1K");
   expect(formatCount(1234)).to.equal("1.234K");
+});
+
+it("parses counts as expected", function () {
+  const langs =
+    "af,af-ZA,ar,ar-AE,ar-BH,ar-DZ,ar-EG,ar-IQ,ar-JO,ar-KW,ar-LB,ar-LY,ar-MA,ar-OM,ar-QA,ar-SA,ar-SY,ar-TN,ar-YE,az,az-AZ,az-AZ,be,be-BY,bg,bg-BG,bs-BA,ca,ca-ES,cs,cs-CZ,cy,cy-GB,da,da-DK,de,de-AT,de-CH,de-DE,de-LI,de-LU,dv,dv-MV,el,el-GR,en,en-AU,en-BZ,en-CA,en-CB,en-GB,en-IE,en-JM,en-NZ,en-PH,en-TT,en-US,en-ZA,en-ZW,eo,es,es-AR,es-BO,es-CL,es-CO,es-CR,es-DO,es-EC,es-ES,es-ES,es-GT,es-HN,es-MX,es-NI,es-PA,es-PE,es-PR,es-PY,es-SV,es-UY,es-VE,et,et-EE,eu,eu-ES,fa,fa-IR,fi,fi-FI,fo,fo-FO,fr,fr-BE,fr-CA,fr-CH,fr-FR,fr-LU,fr-MC,gl,gl-ES,gu,gu-IN,he,he-IL,hi,hi-IN,hr,hr-BA,hr-HR,hu,hu-HU,hy,hy-AM,id,id-ID,is,is-IS,it,it-CH,it-IT,ja,ja-JP,ka,ka-GE,kk,kk-KZ,kn,kn-IN,ko,ko-KR,kok,kok-IN,ky,ky-KG,lt,lt-LT,lv,lv-LV,mi,mi-NZ,mk,mk-MK,mn,mn-MN,mr,mr-IN,ms,ms-BN,ms-MY,mt,mt-MT,nb,nb-NO,nl,nl-BE,nl-NL,nn-NO,ns,ns-ZA,pa,pa-IN,pl,pl-PL,ps,ps-AR,pt,pt-BR,pt-PT,qu,qu-BO,qu-EC,qu-PE,ro,ro-RO,ru,ru-RU,sa,sa-IN,se,se-FI,se-FI,se-FI,se-NO,se-NO,se-NO,se-SE,se-SE,se-SE,sk,sk-SK,sl,sl-SI,sq,sq-AL,sr-BA,sr-BA,sr-SP,sr-SP,sv,sv-FI,sv-SE,sw,sw-KE,syr,syr-SY,ta,ta-IN,te,te-IN,th,th-TH,tl,tl-PH,tn,tn-ZA,tr,tr-TR,tt,tt-RU,ts,uk,uk-UA,ur,ur-PK,uz,uz-UZ,uz-UZ,vi,vi-VN,xh,xh-ZA,zh,zh-CN,zh-HK,zh-MO,zh-SG,zh-TW,zu,zu-ZA".split(
+      ",",
+    );
+
+  langs.forEach(lang => {
+    const testVal = -1234567.89;
+    const formatted = new Intl.NumberFormat(lang).format(testVal);
+    const parsed = parseCount(formatted, lang);
+
+    if (parsed !== testVal) {
+      const parts = new Intl.NumberFormat(lang).formatToParts(testVal);
+      const literal = parts
+        .filter(p => p.type === "literal")
+        .map(part => `U+${part.value.charCodeAt(0).toString(16).padStart(4, "0")}`);
+      expect(parsed).to.equal(
+        testVal,
+        `Failed for locale '${lang}'. Expected '${parsed}' to equal '${testVal}': '${literal.join(", ")}'`,
+      );
+    }
+  });
 });
--- source/format/count.ts
+++ source/format/count.ts
@@ -1,12 +1,41 @@
+import { mustExist } from "../data/nil.js";
+
 /**
  * Convert counts to a human readable string: `1234` → `1.234K`.
  * @param count - The count to format.
+ * @param locale - The locale to use.
  * @returns The formatted string.
  */
-export const formatCount = (count: number | bigint): string => {
-  return new Intl.NumberFormat(undefined, {
+export const formatCount = (count: number | bigint, locale?: string): string => {
+  return new Intl.NumberFormat(locale, {
     compactDisplay: "short",
     maximumFractionDigits: 3,
     notation: "compact",
   }).format(count);
+};
+
+/**
+ * Convert counts from a human readable string back to a number: `1.234K` → `1234`.
+ * @param count - The count to parse.
+ * @param locale - The locale to use.
+ * @returns The number represented by the input string.
+ */
+export const parseCount = (count: string, locale?: string): number => {
+  const format = new Intl.NumberFormat(locale);
+  const parts = format.formatToParts(-12345.6);
+  const numerals = Array.from({ length: 10 }).map((_, i) => format.format(i));
+  const index = new Map(numerals.map((d, i) => [d, i]));
+  const minusSign = new RegExp(`[${mustExist(parts.find(d => d.type === "minusSign")).value}]`);
+  const group = new RegExp(`[${mustExist(parts.find(d => d.type === "group")).value}]`, "g");
+  const decimal = new RegExp(`[${mustExist(parts.find(d => d.type === "decimal")).value}]`);
+  const numeral = new RegExp(`[${numerals.join("")}]`, "g");
+
+  const DIRECTION_MARK = /\u061c|\u200e/g;
+  return +count
+    .trim()
+    .replace(DIRECTION_MARK, "")
+    .replace(group, "")
+    .replace(decimal, ".")
+    .replace(numeral, d => mustExist(index.get(d)).toString())
+    .replace(minusSign, "-");
 };
